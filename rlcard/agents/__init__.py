e44378157aaf229ffe2aaef9fafe500c2844045e
bweithers <bmweithers@gmail.com>
2024-10-10 09:02:13 -0500

    Remove unused distutils import to comply with Python3.12

--- rlcard/agents/__init__.py
+++ rlcard/agents/__init__.py
@@ -1,6 +1,5 @@
 import subprocess
 import sys
-from distutils.version import LooseVersion
 
 reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
